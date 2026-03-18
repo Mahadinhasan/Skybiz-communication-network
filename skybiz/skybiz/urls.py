2835810f9a58080dad3ee805596866c836a8f816
Mehedin Hasan <207408629+Mahadinhasan@users.noreply.github.com>
2026-03-18 06:21:50 +0600

    Update urls.py

--- skybiz/skybiz/urls.py
+++ skybiz/skybiz/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from internet.views import custom_404_view
+
+handler404 = custom_404_view
 
 urlpatterns = [
     path('admin_developer/', admin.site.urls),
